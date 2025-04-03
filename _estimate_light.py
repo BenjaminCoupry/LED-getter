72f17631cb39f019e6c33b106dc91a4c87920fcd
BenjaminCoupry <56168210+BenjaminCoupry@users.noreply.github.com>
2025-04-03 10:33:38 +0200

    fixed call bug

--- _estimate_light.py
+++ _estimate_light.py
@@ -8,23 +8,24 @@ import jax
 
 mode = 'LED'
 
-view_id, mesh_id =3, 3
-ps_images_paths = sorted(glob.glob(f'/media/bcoupry/T7 Shield/Chauvet_1203_matin/PS_{view_id:02d}/DSC_*.NEF'))
-out_path = f'/media/bcoupry/T7 Shield/Chauvet_1203_matin/PS_{view_id:02d}/light_v3_{mode}'
-step = 10
-project_path = f'/media/bcoupry/T7 Shield/Chauvet_1203_matin/meshroom/{mesh_id:02d}'
+for view_id, mesh_id in zip([1,2,3,3,3,3],[1,2,3,4,5,6]):
+    ps_images_paths = sorted(glob.glob(f'/media/bcoupry/T7 Shield/Chauvet_1203_matin/PS_{view_id:02d}/DSC_*.NEF'))
+    out_path = f'/media/bcoupry/T7 Shield/Chauvet_1203_matin/lights/PS_{view_id:02d}/{mode}'
+    step = 10
+    project_path = f'/media/bcoupry/T7 Shield/Chauvet_1203_matin/meshroom/{mesh_id:02d}'
 
-points, normals, pixels, images, validity_mask, mask, shapes, output, optimizer = preprocessing.preprocess(ps_images_paths, sliced=(slice(None,None, step), slice(None,None, step)), threshold=(0.5,0.5), meshroom_project=project_path, storage_device = jax.devices("cpu")[0])
+    points, normals, pixels, images, validity_mask, mask, shapes, output, optimizer = preprocessing.preprocess(ps_images_paths, sliced=(slice(None,None, step), slice(None,None, step)), threshold=(0.5,0.5), meshroom_project=project_path)
 
-if mode=='grid':
-    iterations = {'lambertian':1000}
-    parameters, data, losses, steps = optim_steps.estimate_grid_light(points, normals, images, pixels, shapes, output, optimizer, mask, validity_mask, iterations, 800)
-    outputs.export_results(out_path, mask, parameters, data, losses, steps, images, ps_images_paths)
+    if mode=='grid':
+        iterations = {'lambertian':1000}
+        parameters, data, losses, steps = optim_steps.estimate_grid_light(points, normals, images, pixels, shapes, output, optimizer, mask, validity_mask, iterations, 800)
+        outputs.export_results(out_path, mask, parameters, data, losses, steps, images, ps_images_paths)
 
-elif mode=='LED':
-    iterations = {'directional' : 400, 'rail':300, 'punctual':4000, 'LED' : 4000, 'specular':3000}
-    parameters, data, losses, steps = optim_steps.estimate_physical_light(points, normals, images, pixels, shapes, output, optimizer, mask, validity_mask, iterations)
-    outputs.export_results(out_path, mask, parameters, data, losses, steps, images, ps_images_paths)
+    elif mode=='LED':
+        iterations = {'directional' : 400, 'rail':300, 'punctual':4000, 'LED' : 4000, 'specular':3000}
+        del iterations['rail']
+        parameters, data, losses, steps = optim_steps.estimate_physical_light(points, normals, images, pixels, shapes, output, optimizer, mask, validity_mask, iterations)
+        outputs.export_results(out_path, mask, parameters, data, losses, steps, images, ps_images_paths)
 
 
 #TODO : tester image PNG, avec ou sans distorsion, mesh ou sphere
