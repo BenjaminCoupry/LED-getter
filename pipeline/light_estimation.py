db0f59d6642a8667579b2d295a4619626fcff224
BenjaminCoupry <56168210+BenjaminCoupry@users.noreply.github.com>
2025-08-29 12:06:17 +0200

    fixed for sh light static values

--- pipeline/light_estimation.py
+++ pipeline/light_estimation.py
@@ -10,6 +10,7 @@ import ledgetter.models.values_generator as values_generator
 import ledgetter.optim.gradient_descent as gradient_descent
 import ledgetter.models.models as models
 import ledgetter.utils.files as files
+import ledgetter.utils.functions as functions
 import functools
 
 
@@ -22,6 +23,7 @@ def estimate_light(iterations, pattern, values, images, mask, raycaster, shapes,
     light, renderer, projections = models.get_model(model)
     loss = models.get_loss(light, renderer, delta=delta)
     #parameters, losses_values = jax.jit(functools.partial(gradient_descent.get_gradient_descent(optimizer, loss, iterations, projections=projections, output=output, extra = True, unroll=1), images=images, validity_mask=validity_mask))(parameters, data=data, images=images, validity_mask=validity_mask)
-    parameters, losses_values = jax.jit(gradient_descent.get_gradient_descent(optimizer, loss, iterations, projections=projections, output=output, extra = True, unroll=1))(parameters, data=data, images=images, validity_mask=validity_mask)
+    descent = jax.jit(functools.partial(gradient_descent.get_gradient_descent(optimizer, loss, iterations, projections=projections, output=output, extra = True, unroll=1), data=data))
+    parameters, losses_values = descent(parameters, images=images, validity_mask=validity_mask)
     light_dict = common.update_light_dict(light_dict, name=pattern, losses_values=losses_values, model=model, values=parameters | data, light=light)
     return light_dict, validity_mask
